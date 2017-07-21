f650d6d85a1f3c3b7038666bf9be94cb9aebbb2d
Aidan Steele <aidan.steele@glassechidna.com.au>
2017-07-21 17:22:32 +1000

    init

---
+++ root.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ssm"
+	"strings"
+	"time"
+	"log"
+)
+
+var cfgFile string
+
+var RootCmd = &cobra.Command{
+	Use:   "gossm",
+	Short: "Run commands on remote machines using EC2 SSM Run Command",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		region, _ := cmd.PersistentFlags().GetString("region")
+		profile, _ := cmd.PersistentFlags().GetString("profile")
+		sess := AwsSession(profile, region)
+
+		instance, _ := cmd.PersistentFlags().GetString("instance-id")
+		timeout, _ := cmd.PersistentFlags().GetInt64("timeout")
+		command := strings.Join(args, " ")
+
+		doit(sess, instance, command, timeout)
+	},
+}
+
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gossm.yaml)")
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	RootCmd.PersistentFlags().String("profile", "", "")
+	RootCmd.PersistentFlags().String("region", "", "")
+	RootCmd.PersistentFlags().String("instance-id", "", "")
+	RootCmd.PersistentFlags().Int64("timeout", 600, "")
+}
+
+func initConfig() {
+	if cfgFile != "" { // enable ability to specify config file via flag
+		viper.SetConfigFile(cfgFile)
+	}
+
+	viper.SetConfigName(".gossm") // name of config file (without extension)
+	viper.AddConfigPath("$HOME")  // adding home directory as first search path
+	viper.AutomaticEnv()          // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ run.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ssm"
+	"log"
+	"time"
+	"os"
+)
+
+func AwsSession(profile, region string) *session.Session {
+	sessOpts := session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+		AssumeRoleTokenProvider: stscreds.StdinTokenProvider,
+	}
+
+	if len(profile) > 0 {
+		sessOpts.Profile = profile
+	}
+
+	sess, _ := session.NewSessionWithOptions(sessOpts)
+	config := aws.NewConfig()
+
+	if len(region) > 0 {
+		config.Region = aws.String(region)
+		sess.Config = config
+	}
+
+	return sess
+}
+
+func doit(sess *session.Session, instanceId, command string, timeout int64) {
+	client := ssm.New(sess)
+	resp, err := client.SendCommand(&ssm.SendCommandInput{
+		DocumentName: aws.String("AWS-RunShellScript"),
+		InstanceIds: aws.StringSlice([]string{instanceId}),
+		TimeoutSeconds: &timeout,
+		Parameters: map[string][]*string {
+			"commands": aws.StringSlice([]string{command}),
+		},
+	})
+
+	if err != nil {
+		log.Panicf(err.Error())
+	}
+
+	commandId := resp.Command.CommandId
+	status := "InProgress"
+
+	cmdResp := ssm.GetCommandInvocationOutput{}
+
+	for {
+		resp2, err := client.GetCommandInvocation(&ssm.GetCommandInvocationInput{
+			CommandId: commandId,
+			InstanceId: &instanceId,
+		})
+
+		if err != nil {
+			log.Panicf(err.Error())
+		}
+
+		status = *resp2.Status
+
+		if status != "InProgress" {
+			cmdResp = *resp2
+			break
+		}
+
+		time.Sleep(time.Second * 3)
+	}
+
+	os.Stderr.WriteString(*cmdResp.StandardErrorContent)
+	os.Stdout.WriteString(*cmdResp.StandardOutputContent)
+}
