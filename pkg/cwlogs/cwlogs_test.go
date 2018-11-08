3be026d54e7f060d665e57b8f1d417f778e25425
Aidan Steele <aidan.steele@glassechidna.com.au>
2018-11-08 20:33:12 +1100

    tests for cwlogs

--- pkg/cwlogs/cwlogs_test.go
+++ pkg/cwlogs/cwlogs_test.go
@@ -6,16 +6,17 @@ import (
 	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs/cloudwatchlogsiface"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 type testApi struct {
 	cloudwatchlogsiface.CloudWatchLogsAPI
+	cb func(ctx aws.Context, input *cloudwatchlogs.FilterLogEventsInput, pager func(*cloudwatchlogs.FilterLogEventsOutput, bool) bool, opts ...request.Option) error
 }
 
-func (a *testApi) FilterLogEventsPagesWithContext(aws.Context, *cloudwatchlogs.FilterLogEventsInput, func(*cloudwatchlogs.FilterLogEventsOutput, bool) bool, ...request.Option) error {
-	panic("hiya")
-	return nil
+func (a *testApi) FilterLogEventsPagesWithContext(ctx aws.Context, input *cloudwatchlogs.FilterLogEventsInput, pager func(*cloudwatchlogs.FilterLogEventsOutput, bool) bool, opts ...request.Option) error {
+	return a.cb(ctx, input, pager, opts...)
 }
 
 func TestStream(t *testing.T) {
@@ -26,15 +27,82 @@ func TestStream(t *testing.T) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	input := &cloudwatchlogs.FilterLogEventsInput{
-		LogGroupName: aws.String("/aws/ssm/AWS-RunShellScript"),
+		LogGroupName:        aws.String("/aws/ssm/AWS-RunShellScript"),
 		LogStreamNamePrefix: &commandId,
 	}
 
-	stream := c.Stream(ctx, input)
+	var stream *cwStream
 
-	for msg := range stream.Channel {
-		msg.
+	// this is grotesque
+	api.cb = func(ctx aws.Context, pageInput *cloudwatchlogs.FilterLogEventsInput, pager func(*cloudwatchlogs.FilterLogEventsOutput, bool) bool, opts ...request.Option) error {
+		assert.EqualValues(t, pageInput, input)
+		pager(&cloudwatchlogs.FilterLogEventsOutput{
+			Events: []*cloudwatchlogs.FilteredLogEvent{
+				{
+					LogStreamName: aws.String("/aws/ssm/AWS-RunShellScript/83b98484-4a9b-4470-ab17-e4a646e2a72e/i-abc123/aws-runShellScript/stdout"),
+					EventId: aws.String("ev10"),
+					Timestamp: aws.Int64(50),
+				},
+				{
+					LogStreamName: aws.String("/aws/ssm/AWS-RunShellScript/83b98484-4a9b-4470-ab17-e4a646e2a72e/i-abc123/aws-runShellScript/stdout"),
+					EventId: aws.String("ev11"),
+					Timestamp: aws.Int64(55),
+				},
+				{
+					LogStreamName: aws.String("/aws/ssm/AWS-RunShellScript/83b98484-4a9b-4470-ab17-e4a646e2a72e/i-def456/aws-runShellScript/stdout"),
+					EventId: aws.String("ev20"),
+					Timestamp: aws.Int64(60),
+				},
+				{
+					LogStreamName: aws.String("/aws/ssm/AWS-RunShellScript/83b98484-4a9b-4470-ab17-e4a646e2a72e/i-def456/aws-runShellScript/stdout"),
+					EventId: aws.String("ev21"),
+					Timestamp: aws.Int64(65),
+				},
+			},
+		}, true)
+
+		api.cb = func(ctx aws.Context, pageInput *cloudwatchlogs.FilterLogEventsInput, pager func(*cloudwatchlogs.FilterLogEventsOutput, bool) bool, opts ...request.Option) error {
+			assert.EqualValues(t, 56, *pageInput.StartTime)
+
+			stream.Ignore("/aws/ssm/AWS-RunShellScript/83b98484-4a9b-4470-ab17-e4a646e2a72e/i-abc123/aws-runShellScript/stdout")
+
+			pager(&cloudwatchlogs.FilterLogEventsOutput{
+				Events: []*cloudwatchlogs.FilteredLogEvent{
+					{
+						LogStreamName: aws.String("/aws/ssm/AWS-RunShellScript/83b98484-4a9b-4470-ab17-e4a646e2a72e/i-abc123/aws-runShellScript/stdout"),
+						EventId: aws.String("ev12"),
+						Timestamp: aws.Int64(58),
+					},
+					{
+						LogStreamName: aws.String("/aws/ssm/AWS-RunShellScript/83b98484-4a9b-4470-ab17-e4a646e2a72e/i-def456/aws-runShellScript/stdout"),
+						EventId: aws.String("ev22"),
+						Timestamp: aws.Int64(67),
+					},
+					{
+						LogStreamName: aws.String("/aws/ssm/AWS-RunShellScript/83b98484-4a9b-4470-ab17-e4a646e2a72e/i-def456/aws-runShellScript/stdout"),
+						EventId: aws.String("ev23"),
+						Timestamp: aws.Int64(69),
+					},
+				},
+			}, true)
+
+			api.cb = func(ctx aws.Context, pageInput *cloudwatchlogs.FilterLogEventsInput, pager func(*cloudwatchlogs.FilterLogEventsOutput, bool) bool, opts ...request.Option) error {
+				assert.EqualValues(t, 70, *pageInput.StartTime)
+				cancel()
+				return nil
+			}
+
+			return nil
+		}
+		return nil
 	}
 
-	cancel()
+	stream = c.Stream(ctx, input)
+
+	idx := 0
+	expectedEventIds := []string{"ev10", "ev11", "ev20", "ev21", "ev22", "ev23"}
+	for ev := range stream.Channel {
+		assert.EqualValues(t, expectedEventIds[idx], *ev.EventId)
+		idx++
+	}
 }
